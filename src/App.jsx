3ae07d071dca9b547d52aa56d6c7bab57b838d13
Amanda Alves <amandaalves51030@gmail.com>
2023-09-01 16:23:28 -0300

    adicionado imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import tmdb from './tmdb'
+import MovieRow from './components/MovieRow';
 
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(() => {
     const loadAll= async () =>{
       //Pegando Lista Inteira
       let list = await tmdb.getHomeList();
-      console.log(list)
+      setMovieList(list);
     } 
   loadAll();
   }, []);
 
   return (
-    <h2>Ola mundo</h2>
+    <div className='page'>
+     <section className='lists'>
+      {movieList.map((item, key)=>(
+       <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+     </section>
+    </div>  
   )
 }
 
